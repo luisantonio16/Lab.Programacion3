e1a7ee738b7c9759bbda98132c44a28d9e14c3b9
luisantonio16 <luis16.16m@gmail.com>
2023-05-16 12:47:55 -0400

    update

--- js/script.js
+++ js/script.js
@@ -11,15 +11,44 @@ const submitBtn = document.querySelector(".submit");
 const progressText = document.querySelectorAll(".step p");
 const progressCheck = document.querySelectorAll(".step .check");
 const bullet = document.querySelectorAll(".step .bullet");
-let current = 1;
+const txtNombre = document.querySelector('.txtNombre');
+const txtApellido = document.querySelector('.txtApellido');
+const txtFechaNaciemiento = document.querySelector('.txtFechaNaci');
+const txtDireccion = document.querySelector('.txtDireccion');
+const txtTiposangre = document.querySelector('.txtTipoSangre');
+const txtNombreFamiliar = document.querySelector('.txtNombreFamiliar');
+const txtParentesco = document.querySelector('.txtParentesco');
+const txtEdad = document.querySelector('.txtEdad');
+const txtNum = document.querySelector('.txtNum');
+const txtEnfermedad = document.querySelector('.txtEnfermedad');
+const txtTiempo = document.querySelector('.txtTiempo');
+const txtDetalle = document.querySelector('.txtDetalle');
+const txtFechaInterno = document.querySelector('.txtFechaInterno');
+const txtHospital = document.querySelector('.txtHospital');
+const txtDiagnostico = document.querySelector('.txtDiagnostico');
+const tabla = document.querySelector('.table-body');
+const subTitle = document.querySelector('.subtitle');
+const btnGuardar = document.querySelector('.btnEnviar');
 
-nextBtnFirst.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-25%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
+
+const input = document.querySelectorAll('txtNom');
+let current = 1;
+let objetoPersona = [];
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  if(localStorage.getItem('Pacientes')){
+    objetoPersona  = JSON.parse(localStorage.getItem('Pacientes')) || [];
+    pruebafuction(objetoPersona);
+     
+}  
+});
+nextBtnFirst.addEventListener("click", (e)=>{
+    e.preventDefault();
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
 });
 nextBtnSec.addEventListener("click", function(event){
   event.preventDefault();
@@ -72,4 +101,95 @@ prevBtnFourth.addEventListener("click", function(event){
   progressText[current - 2].classList.remove("active");
   current -= 1;
 });
+
+
+
+function guardar() {
+  const objeto  = {
+      id:   Math.floor(Math.random() * 1000),
+      nombrePaciente: txtNombre.value,
+      Apellido: txtApellido.value,
+      fechanacimiento: txtFechaNaciemiento.value,
+      direccion: txtDireccion.value,
+      tipoSangre: txtTiposangre.value,
+      nombreFamiliar: txtNombreFamiliar.value,
+      parentesco: txtParentesco.value,
+      edad: txtEdad.value,
+      numero: txtNum.value,
+      enfermedad: txtEnfermedad.value,
+      tiempo: txtTiempo.value,
+      detalle: txtDetalle.value,
+      fechaInterno: txtFechaInterno.value,
+      hospital: txtHospital.value,
+      diagnostico: txtDiagnostico.value
+
+  } 
+
+  const existe = objetoPersona.some(pro => pro.id === objeto.id);
+  if (existe) {
+    
+      objetoPersona = [...objeto];
+      
+  }else{
+    objetoPersona = [...objetoPersona, objeto];
+  }
+  console.log(existe);
+
+  pruebafuction(objetoPersona);
+  localStorage.setItem('Pacientes' , JSON.stringify(objeto));
+
+}
+
+function pruebafuction(objeto) {
+  tabla.innerHTML = '';
+  objeto.forEach(element => {
+     tabla.innerHTML += `
+     <tr>
+     <td>${element.nombrePaciente + element.Apellido}</td>
+     <td>${element.direccion}</td>
+     <td>${element.tipoSangre}</td>
+     <td>${ element.nombreFamiliar}</td>
+     <td>${element.tiempo}</td>
+     <td>${element.hospital}</td>
+     <td>
+       <button data-id="${element.id}" class="btn-editar">Editar</button>
+     </td>
+  </tr>
+ `        
+});
+
+
+}
+
+btnGuardar.addEventListener('click', ()=>{
+  guardar();
+});
+
+function validar() {
+  if(input.length == 0 ){
+    subTitle.innerHTML = `Todos los Campos son Requeridos`;
+    return false;
+}
+return true;
+}
+
+function btnAccion(e){
+  if(e.target.classList.contains('btn-editar')){
+      const id = e.target.dataset.id;
+    const existe = objetoPersona.some(obj=>  obj.id === id);
+    console.log(existe);
+    console.log(id);
+    console.log(existe.id);
+    if(existe){
+    console.log(existe);
+  
+    }   
+}
+}
+
+tabla.addEventListener('click', e =>{
+  btnAccion(e);
+ //console.log(e.target);
+});
+
 
